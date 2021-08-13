e4a7f4cc4402658a2b1b8b795339983958c12fbc
Olmeiro Orozco <olme.orozco@gmail.com>
2021-08-13 12:14:50 -0500

    docs

---
+++ docs/precache-manifest.85a70042c019b2b08613ddfbe6751ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03411fba8e5f18a42c1c0fe30215d1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5f2dc5a7b2bae9bc80",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5f2dc5a7b2bae9bc80",
+    "url": "/static/js/main.5c51f422.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
